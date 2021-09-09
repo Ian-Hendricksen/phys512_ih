3ac6a5147860da4f2bf5bb2bc98781f795cc8729
Ian Hendricksen <ihendricksen42@gmail.com>
2021-09-09 18:30:45 -0400

    wip

--- problem_sets/ps1.py
+++ problem_sets/ps1.py
@@ -11,12 +11,34 @@ import matplotlib.pyplot as plt
 #-----------------------------------------------------------------------------
 # (Q1)
 
-# will this show up on the internets?
+# (a)
+
+
+
+# (b)
+
+
 
 #-----------------------------------------------------------------------------
 # (Q2)
 
-
+def ndiff(fun, x, full=False):
+    
+    eps = 1e-7 # Assumption
+    xc = x # But what if x = 0? --> worry later
+    dx = eps**(1/3)*xc # Following from Numerical Recipes
+        
+    temp = x + dx
+    dx = temp - x
+        
+    fp = (fun(temp) - fun(x-dx))/2*dx
+        
+    err = 10
+    
+    if full==False:
+        return fp
+    else:
+        return fp, dx, err
 
 #-----------------------------------------------------------------------------
 # (Q3)
