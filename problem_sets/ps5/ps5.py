4c56cf4eff5cade2afd4c0236967a32501256851
Ian Hendricksen <ihendricksen42@gmail.com>
2021-10-28 19:54:55 -0400

    wip

--- problem_sets/ps5/ps5.py
+++ problem_sets/ps5/ps5.py
@@ -32,27 +32,25 @@ def shift(y, dy):
     N = len(y)    
     y_ft = np.fft.fft(y)
     
-    imp = np.zeros(N)
-    imp[dy] = 1 
-    imp_ft = np.fft.fft(imp)
+    delta = np.zeros(N)
+    delta[dy-1] = 1 
+    delta_ft = np.fft.fft(delta)
     
-    conv = np.fft.ifft(imp_ft * y_ft)
+    conv = np.fft.ifft(delta_ft * y_ft)
     return conv
     
 def gauss(x, mu, sigma, amp, offset):
     return offset + amp*np.exp(-(x - mu)**2/(2*sigma**2))
 
-x1 = np.linspace(-5, 5, 1000) # x1 --> "x for problem 1"
-g1 = gauss(x1, 0, 0.5, 1, 0) # g1 --> "gauss for problem 1"
-g1_shift = shift(g1, int(len(x1)/2) + 250)
+x1 = np.linspace(-5, 5, 10000) # x1 --> "x for problem 1"
+g1 = gauss(x1, -2.5, 0.5, 1, 0) # g1 --> "gauss for problem 1"
+g1_shift = shift(g1, int(len(x1)/2))
 
 # f1 = plt.figure()
 # plt.plot(x1, g1, label = 'Initial Gaussian')
-# plt.plot(x1, abs(g1_shift.real), label = 'Shifted Gaussian')
+# plt.plot(x1, abs(g1_shift), label = 'Shifted Gaussian')
 # plt.legend()
 # plt.savefig('gauss_shift.png')
-
-print('This currently shifts by 2.5 not by 5 - FIX!')
 
 #-----------------------------------------------------------------------------
 # (Q2)
@@ -64,15 +62,16 @@ def corr(f, g):
     f_ft = np.fft.fft(f)
     g_ft_conj = np.conj(np.fft.fft(g))
     fg_corr = np.fft.ifft(f_ft * g_ft_conj)
-    return np.fft.fftshift(fg_corr)
+    return fg_corr
 
 x2 = np.linspace(-10, 10, 1000)
-g2 = gauss(x2, 0, 2, 10, 0)
+g2 = gauss(x2, 0, 2, 0.1, 0)
 g_corr = corr(g2, g2)
 
 # f2 = plt.figure()
-# plt.plot(x2, g2/max(g2), label = 'Gaussian') # Normalized
-# plt.plot(x2, abs(g_corr)/max(abs(g_corr)), label = 'Correlation of Gaussian with Itself')
+# plt.plot(x2, g2, label = 'Gaussian')
+# g_corr = np.fft.fftshift(g_corr)
+# plt.plot(x2, abs(g_corr), label = 'Correlation of Gaussian with Itself')
 # plt.legend()
 # plt.savefig('gauss_corr.png')
     
@@ -83,14 +82,16 @@ print('=======================')
 print('(Q3)')
 
 x3 = np.linspace(-5, 5, 1000)
-g3 = gauss(x1, 0, 0.5, 1, 0)
-g3_shift = shift(g3, int(len(x3)/2) + 250)
-g3_shift_corr = corr(g3, g3_shift)
+g3 = gauss(x3, -2.5, 0.5, 0.1, 0)
+g3_shift = shift(g3, int(len(x3)/25))
+g3_shift_corr = corr(g3_shift, g3_shift)
 
 # f3 = plt.figure()
-# plt.plot(x3, g3)
-# plt.plot(x3, abs(g3_shift)/max(abs(g3_shift)))
-# plt.plot(x3, abs(g3_shift_corr)/max(abs(g3_shift_corr)))
+# plt.plot(x3, g3, label = 'Original Gaussian')
+# plt.plot(x3, abs(g3_shift), label = 'Shifted Gaussian')
+# plt.plot(x3, abs(g3_shift_corr), label = 'Shifted Gaussian Correlated with Itself')
+# plt.legend()
+# plt.savefig('gauss_shift_corr.png')
 
 #-----------------------------------------------------------------------------
 # (Q4)
@@ -121,14 +122,15 @@ def conv_safe(f, g, N_zeros):
     return conv
 
 x4 = np.linspace(-10, 10, 1000)
-g4 = gauss(x4, 0, 2, 1, 0)
-N_zeros = 10
+g4 = gauss(x4, 0, 2, 0.1, 0)
+N_zeros = 1000
 g4_conv = conv_safe(g4, g4, N_zeros) # maybe want to try 2 different gaussians?
 x4_extd = np.linspace(min(x4), max(x4) + N_zeros, len(x4) + N_zeros)
 
 # f4 = plt.figure()
-# plt.plot(x4, g4)
-# plt.plot(x4_extd, g4_conv)
+# plt.plot(abs(g4))
+# plt.plot(abs(g4_conv))
+# plt.savefig('conv_safe.png')
 
 #-----------------------------------------------------------------------------
 # (Q5)
@@ -158,11 +160,18 @@ def sin_ft_anl(y, k): # "sin fourier transform analytical"
 
 anl_sin_ft = sin_ft_anl(sin, k)
 
-f5c = plt.figure()
-plt.plot(abs(sin_ft), label = 'FFT') # Negative frequencies aliased to high frequencies
-plt.plot(abs(anl_sin_ft), label = 'Analytical')
-plt.legend()
-plt.savefig('sin_FFT_and_analytical.png')
+# f5c = plt.figure()
+# plt.subplot(1,2,1)
+# plt.plot(abs(sin_ft), label = 'FFT') # Negative frequencies aliased to high frequencies
+# plt.plot(abs(anl_sin_ft), label = 'Analytical')
+# plt.legend()
+
+# plt.subplot(1,2,2)
+# plt.plot(abs(sin_ft), label = 'FFT') # Negative frequencies aliased to high frequencies
+# plt.plot(abs(anl_sin_ft), label = 'Analytical')
+# plt.xlim(40, 60)
+# plt.legend()
+# plt.savefig('sin_FFT_and_analytical.png')
 
 fqs5 = np.arange(len(x5))
 
@@ -175,13 +184,26 @@ def cos_wind(x):
 sin_win = sin*cos_wind(x5)
 sin_win_ft = np.fft.fft(sin_win)
     
-# Things are currently not very well normalized here. This goes for the
-# entire script really. Need to fix this
+# f5d = plt.figure()
+# plt.plot(abs(sin_ft)/len(sin_ft), label = 'No window')
+# plt.plot(abs(sin_win_ft)/len(sin_win_ft), label = 'With window')
+# plt.legend()
 
-f5d = plt.figure()
-plt.plot(abs(sin_ft)/len(sin_ft), label = 'No window')
-plt.plot(abs(sin_win_ft)/len(sin_win_ft), label = 'With window')
-plt.legend()
+cos_wind_ft = np.fft.fft(cos_wind(x5))
+N_cos_wind = len(cos_wind(x5))
+
+f = open('cos_wind_ft.txt', 'w')
+f.write('DFT of Cosine Window Function:')
+f.write(f'\nN = {N_cos_wind}')
+f.write(f'\nThe first element in the DFT of the window is {abs(cos_wind_ft[0])}, which should be N/2 = {N_cos_wind/2}')
+f.write(f'\nThe second element in the DFT of the window is {abs(cos_wind_ft[1])}, which should be N/4 = {N_cos_wind/4}')
+f.write(f'\nThe last element in the DFT of the window is {abs(cos_wind_ft[-1])}, which should be N/4 = {N_cos_wind/2}')
+f.write('\nThe rest of the elements in the DFT should be ~0, and are:')
+f.write(f'\n{abs(cos_wind_ft)[1:-1]}')
+f.close()
+
+f5e = plt.figure()
+plt.plot(abs(cos_wind_ft))
 
 #-----------------------------------------------------------------------------
 # (Q6)
@@ -195,6 +217,6 @@ rand_walk = np.cumsum(np.random.randn(n))
 rand_walk_ft = np.fft.fft(rand_walk * cos_wind(x6))
 rand_walk_ft = np.delete(rand_walk_ft, 0) # remove k=0
 
-f6 = plt.figure()
-plt.plot(abs(rand_walk_ft))
+# f6 = plt.figure()
+# plt.plot(abs(rand_walk_ft))
 # plt.plot(max(abs(rand_walk_ft))*(1/np.arange(n)**2))
