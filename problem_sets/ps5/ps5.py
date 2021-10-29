147f726ad51fb479695e0fc0e59ba4de09c0f7ae
Ian Hendricksen <ihendricksen42@gmail.com>
2021-10-29 18:59:37 -0400

    completed

--- problem_sets/ps5/ps5.py
+++ problem_sets/ps5/ps5.py
@@ -83,8 +83,8 @@ print('(Q3)')
 
 x3 = np.linspace(-5, 5, 1000)
 g3 = gauss(x3, -2.5, 0.5, 0.1, 0)
-g3_shift = shift(g3, int(len(x3)/25))
-g3_shift_corr = corr(g3_shift, g3_shift)
+g3_shift = shift(g3, int(len(x3)/4))
+g3_shift_corr = corr(g3, g3_shift)
 
 # f3 = plt.figure()
 # plt.plot(x3, g3, label = 'Original Gaussian')
@@ -138,7 +138,49 @@ x4_extd = np.linspace(min(x4), max(x4) + N_zeros, len(x4) + N_zeros)
 print('=======================')
 print('(Q5)')
 
-x5 = np.linspace(-500, 500, 1000)
+print('------')
+print('(b)')
+
+def multiples(N):
+    mults = []
+    not_mults = np.arange(1,N+1)
+    for i in range(1, N):
+        div = f'{N/i}'
+        intg, rem = div.split('.')
+        if float(rem) == 0:
+            mults.append(int(intg))
+    mults.append(1)
+    mults = np.array(mults[::-1])
+    index = []
+    for i in range(len(mults)):
+        index.append(np.where(not_mults == mults[i])[0][0])
+    not_mults = np.delete(not_mults, index)
+    return mults, not_mults
+
+def complex_sum(k, N):
+    J = complex(0, 1)
+    num = 1 - np.cos(2*np.pi*k) + J*np.sin(2*np.pi*k)
+    den = 1 - np.cos(2*np.pi*k/N) + J*np.sin(2*np.pi*k/N)
+    return num/den
+
+N = 100
+k_mults, k_not_mults = multiples(N)
+f = open('non_multiples_k.txt', 'w')
+f.write(f'N = {N}')
+f.write(f'\nThe multiples of N are {k_mults}')
+f.write(f'\nThe non-multiple integers of N are {k_not_mults}')
+f.write('\n==========================================================')
+f.write('\nThe following are the values of the sum from question 5(a)')
+f.write('\nfor integers k that are not a multiple of N:')
+f.write('\n')
+for i in range(len(k_not_mults)):
+    f.write(f'\n{abs(complex_sum(k_not_mults[i], N))}')
+f.close()
+        
+print('------')
+print('(c)')
+
+x5 = np.arange(100)
 k = 50.125
 sin = np.sin(2*np.pi*k*x5/len(x5))
 sin_ft = np.fft.fft(sin) # np.fft.fftshift(np.fft.fft(sin))
@@ -161,22 +203,16 @@ def sin_ft_anl(y, k): # "sin fourier transform analytical"
 anl_sin_ft = sin_ft_anl(sin, k)
 
 # f5c = plt.figure()
-# plt.subplot(1,2,1)
 # plt.plot(abs(sin_ft), label = 'FFT') # Negative frequencies aliased to high frequencies
 # plt.plot(abs(anl_sin_ft), label = 'Analytical')
-# plt.legend()
-
-# plt.subplot(1,2,2)
-# plt.plot(abs(sin_ft), label = 'FFT') # Negative frequencies aliased to high frequencies
-# plt.plot(abs(anl_sin_ft), label = 'Analytical')
-# plt.xlim(40, 60)
 # plt.legend()
 # plt.savefig('sin_FFT_and_analytical.png')
 
-fqs5 = np.arange(len(x5))
+print('Standard deviation between FFT and analytical DFT is ', np.std(abs(sin_ft) - abs(anl_sin_ft)))
+print('Average residual is ', np.mean(abs(sin_ft) - abs(anl_sin_ft)))
 
-print('Difference in peak values b/w fft and analytical is', fqs5[np.argmax(abs(sin_ft[0:int(len(fqs5)/2)]))]
-      - fqs5[np.argmax(abs(anl_sin_ft[0:int(len(fqs5)/2)]))])
+print('------')
+print('(d)')
 
 def cos_wind(x):
     return 0.5-0.5*np.cos(2*np.pi*x/len(x))
@@ -185,9 +221,13 @@ sin_win = sin*cos_wind(x5)
 sin_win_ft = np.fft.fft(sin_win)
     
 # f5d = plt.figure()
-# plt.plot(abs(sin_ft)/len(sin_ft), label = 'No window')
-# plt.plot(abs(sin_win_ft)/len(sin_win_ft), label = 'With window')
+# plt.plot(abs(sin_ft), label = 'No window')
+# plt.plot(abs(sin_win_ft), label = 'With window')
 # plt.legend()
+# plt.savefig('sin_FFT_window.png')
+
+print('------')
+print('(e)')
 
 cos_wind_ft = np.fft.fft(cos_wind(x5))
 N_cos_wind = len(cos_wind(x5))
@@ -197,13 +237,21 @@ f.write('DFT of Cosine Window Function:')
 f.write(f'\nN = {N_cos_wind}')
 f.write(f'\nThe first element in the DFT of the window is {abs(cos_wind_ft[0])}, which should be N/2 = {N_cos_wind/2}')
 f.write(f'\nThe second element in the DFT of the window is {abs(cos_wind_ft[1])}, which should be N/4 = {N_cos_wind/4}')
-f.write(f'\nThe last element in the DFT of the window is {abs(cos_wind_ft[-1])}, which should be N/4 = {N_cos_wind/2}')
+f.write(f'\nThe last element in the DFT of the window is {abs(cos_wind_ft[-1])}, which should be N/4 = {N_cos_wind/4}')
 f.write('\nThe rest of the elements in the DFT should be ~0, and are:')
-f.write(f'\n{abs(cos_wind_ft)[1:-1]}')
+f.write(f'\n{abs(cos_wind_ft)[2:-1]}')
 f.close()
 
-f5e = plt.figure()
-plt.plot(abs(cos_wind_ft))
+sin_neighb_ft = np.zeros(N_cos_wind)
+for i in range(1, N_cos_wind-1):
+    sin_neighb_ft[i] = -(1/4)*abs(sin_ft)[i-1] + (1/2)*abs(sin_ft)[i] + -(1/4)*abs(sin_ft)[i+1]
+
+f5d = plt.figure()
+plt.plot(abs(sin_ft), label = 'No window')
+plt.plot(abs(sin_win_ft), label = 'With window')
+plt.plot(abs(sin_neighb_ft), label = 'No window scaled by neighbors')
+plt.legend()
+plt.savefig('sin_ft_neighbors.png')
 
 #-----------------------------------------------------------------------------
 # (Q6)
@@ -216,7 +264,11 @@ x6 = np.arange(n) # the random walk has x-axis as just integer steps
 rand_walk = np.cumsum(np.random.randn(n))
 rand_walk_ft = np.fft.fft(rand_walk * cos_wind(x6))
 rand_walk_ft = np.delete(rand_walk_ft, 0) # remove k=0
+rand_walk_ps = abs(rand_walk_ft)**2
 
-# f6 = plt.figure()
-# plt.plot(abs(rand_walk_ft))
-# plt.plot(max(abs(rand_walk_ft))*(1/np.arange(n)**2))
+f6 = plt.figure()
+plt.plot(rand_walk_ps, label = 'Random Walk PS')
+plt.plot((1/np.arange(1, n)**2)*max(rand_walk_ps), label = 'Scaled $1/k^2$')
+plt.xlim(-1, 20)
+plt.legend()
+# plt.savefig('rw_ps.png')
